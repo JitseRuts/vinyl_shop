9a6f6cd2e2844ed6c64abad021b6c1afc41e77fb
Jitse Ruts <r0842934@student.thomasmore.be>
2021-10-11 10:36:34 +0200

    Oefeningen maken en mix

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
     <title>@yield('title', 'The Vinyl Shop')</title>
+    @include('shared.icons')
 </head>
 <body>
 @include('shared/navigation')
@@ -12,7 +13,6 @@
     @yield('main', 'Page under construction ...')
 </main>
 @include('shared/footer')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.js"></script>
+<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
